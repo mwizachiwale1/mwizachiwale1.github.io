842c563d4d93a87977a562350deb1c58013061ba
mwizachiwale1 <47919799+mwizachiwale1@users.noreply.github.com>
2019-09-30 06:16:38 +0200

    Folder for authentication

---
+++ authentication/data_base/authentication.cs
@@ -0,0 +1,63 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+namespace Ibas_accounts
+{
+    #region Authentication
+    public class Authentication
+    {
+        #region Member Variables
+        protected int _id;
+        protected string _username;
+        protected string _password;
+        protected string _profile_photo;
+        protected string _position;
+        protected int _pv_position;
+        #endregion
+        #region Constructors
+        public Authentication() { }
+        public Authentication(string username, string password, string profile_photo, string position, int pv_position)
+        {
+            this._username=username;
+            this._password=password;
+            this._profile_photo=profile_photo;
+            this._position=position;
+            this._pv_position=pv_position;
+        }
+        #endregion
+        #region Public Properties
+        public virtual int Id
+        {
+            get {return _id;}
+            set {_id=value;}
+        }
+        public virtual string Username
+        {
+            get {return _username;}
+            set {_username=value;}
+        }
+        public virtual string Password
+        {
+            get {return _password;}
+            set {_password=value;}
+        }
+        public virtual string Profile_photo
+        {
+            get {return _profile_photo;}
+            set {_profile_photo=value;}
+        }
+        public virtual string Position
+        {
+            get {return _position;}
+            set {_position=value;}
+        }
+        public virtual int Pv_position
+        {
+            get {return _pv_position;}
+            set {_pv_position=value;}
+        }
+        #endregion
+    }
+    #endregion
+}
---
+++ authentication/data_base/authentication.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Export to PHP Array plugin for PHPMyAdmin
+ * @version 4.8.4
+ */
+
+/**
+ * Database `ibas_accounts`
+ */
+
+/* `ibas_accounts`.`authentication` */
+$authentication = array(
+  array('id' => '1','username' => 'Jane Doe','password' => 'janedoe','profile_photo' => 'jane_doe.jpg','position' => 'Cashier','pv_position' => '1'),
+  array('id' => '2','username' => 'Mumba Chewe','password' => 'mumbachewe','profile_photo' => 'mumba_chewe.jpg','position' => 'Accountant','pv_position' => '2'),
+  array('id' => '3','username' => 'Robert Bwalya','password' => 'robertbwalya','profile_photo' => 'robert_bwalya.jpg','position' => 'Senior Accountant','pv_position' => '3')
+);

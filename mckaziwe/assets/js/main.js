7ce37db154e518fb73b609889b491eb4f17f269b
mwizachiwale1 <47919799+mwizachiwale1@users.noreply.github.com>
2023-04-08 16:31:33 +0200

    Altered dynamic text and added bar color

--- mckaziwe/assets/js/main.js
+++ mckaziwe/assets/js/main.js
@@ -3,10 +3,7 @@ var statmentStage = 0;
 var index = 0;
 $(document).ready(function() {
   const text = $('#dynamic-text'); // where text is displayed
-  let sentence0 = ['C', 'o', 'n', 'c', 'e', 'i', 'v', 'e', 's', ','];
-  let sentence1 = ['','D', 'e', 's', 'i', 'g', 'n', 's', ','];
-  let sentence2 = ['', '&',' ', 'P', 'r', 'o', 'd', 'u', 'c', 'e', 's', '.', '.', '.'];
-  let sentence3 = ['D', 'i', 'g', 'i', 't', 'a', 'l', ' ', 'C', 'o', 'n', 't', 'e', 'n', 't', '.'];
+  let sentence0 = ['B', 'r', 'e', 'a', 't', 'h', 'i', 'n', 'g',' ', 'i', 'n', ' ', 'y', 'o', 'u', 'r', ' ', 'm', 'o', 'm', 'e', 'n', 't'];
   // Creation and destruction of tags
 
   function write(len, sentence){
@@ -28,7 +25,7 @@ $(document).ready(function() {
           setTimeout(function(){
             $("#id"+(len - (index - len) - 1)).remove();
             index++;
-          }, 1000);
+          }, 1200);
         } else {
             $("#id"+(len - (index - len) - 1)).remove();
             index++;
@@ -49,25 +46,13 @@ $(document).ready(function() {
       let len = sentence0.length;
       write(len, sentence0);
 
-    } else if(statementNo == 1){
-       // statment 1
-       let len = sentence1.length;
-        write(len, sentence1);
-    } else if(statementNo == 2){
-      // statment 2
-      let len = sentence2.length;
-      write(len, sentence2);
-    } else if(statementNo == 3){
-      // statment 3
-      let len = sentence3.length;
-      write(len, sentence3);
     } else{
         statementNo = 0;
         statmentStage = 0;
     }
   }
   
-  var r = setInterval(print, 220, window.column);
+  var r = setInterval(print, 230, window.column);
   
   // text.innerHTML += sentence[window.row][window.column];
 })

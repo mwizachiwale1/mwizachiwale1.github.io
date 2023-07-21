7ed439666a0f52b076bc02b4ca4d834a9126b494
mwizachiwale1 <47919799+mwizachiwale1@users.noreply.github.com>
2023-07-21 03:55:59 +0200

    Added CMS site

---
+++ dojo-blog/src/App.js
@@ -0,0 +1,16 @@
+import Navbar from './Navbar';
+import Home from './Home';
+
+function App() {
+  const title = "Welcome to the new blog";
+  return (
+    <div className="App">
+      <Navbar />
+      <div className="content">
+        <Home></Home>
+      </div>
+    </div>
+  );
+}
+
+export default App;
